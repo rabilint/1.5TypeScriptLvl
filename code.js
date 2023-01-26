d3e8165284f85ba5740df2146b9c06ba21c7f054
rabilint <rabilint2007@gmail.com>
2023-01-26 16:24:19 +0200

    ggg

--- code.js
+++ code.js
@@ -1,31 +1,33 @@
+import * as moment from 'node_modules/moment/min/moment.min.js';
+
 let can = false;
-console.log("1");
+console.log('1');
 function myFO() {
   if (can === false) {
-    let value2 = document.getElementById("input2").value;
-    if (value2 == NaN) {
+    let value2 = document.getElementById('input2').value;
+    if (isNaN(value2)) {
       value2 = 0;
     }
     can = true;
-    timePlus = moment().add(value2, "minute");
-    time = moment();
-    myVarPlus = moment.duration(timePlus).asSeconds();
-    myVar = moment.duration(time).asSeconds();
-    timer = myVarPlus - myVar;
-    myTimer = setInterval(() => {
+    const timePlus = moment().add(value2, 'minute');
+    let time = moment();
+    const myVarPlus = moment.duration(timePlus).asSeconds();
+    const myVar = moment.duration(time).asSeconds();
+    let timer = myVarPlus - myVar;
+    const myTimer = setInterval(() => {
       if (timer > 0) {
         if (timer * 1000 > 86400000) {
-          time = moment.utc(timer * 1000).format("d:h:mm:ss");
+          time = moment.utc(timer * 1000).format('d:h:mm:ss');
         } else if (timer * 1000 > 3600000) {
-          console.log("3");
-          time = moment.utc(timer * 1000).format("h:mm:ss");
+          console.log('3');
+          time = moment.utc(timer * 1000).format('h:mm:ss');
         } else if (timer * 1000 < 3600000) {
-          time = moment.utc(timer * 1000).format("mm:ss");
+          time = moment.utc(timer * 1000).format('mm:ss');
         }
-        document.getElementById("demo").innerHTML = time;
-        timer = timer - 1;
+        document.getElementById('demo').innerHTML = time;
+        timer -= 1;
       } else {
-        document.getElementById("demo").innerHTML = timer;
+        document.getElementById('demo').innerHTML = timer;
         clearInterval(myTimer);
         can = false;
         return can;
@@ -33,31 +35,31 @@ function myFO() {
     }, 1000);
   } else {
     clearInterval(myTimer);
-    let value2 = document.getElementById("input2").value;
-    if (value2 == NaN) {
+    let value2 = document.getElementById('input2').value;
+    if (isNaN(value2)) {
       value2 = 0;
     }
     can = true;
-    timePlus = moment().add(value2, "minute");
-    time = moment();
-    myVarPlus = moment.duration(timePlus).asSeconds();
-    myVar = moment.duration(time).asSeconds();
-    timer = myVarPlus - myVar;
-    document.getElementById("demo").innerHTML = 0;
-    myTimer = setInterval(() => {
+    const timePlus = moment().add(value2, 'minute');
+    let time = moment();
+    const myVarPlus = moment.duration(timePlus).asSeconds();
+    const myVar = moment.duration(time).asSeconds();
+    let timer = myVarPlus - myVar;
+    document.getElementById('demo').innerHTML = 0;
+    const myTimer = setInterval(() => {
       if (timer > 0) {
         if (timer * 1000 > 86400000) {
-          time = moment.utc(timer * 1000).format("d:h:mm:ss");
+          time = moment.utc(timer * 1000).format('d:h:mm:ss');
         } else if (timer * 1000 > 3600000) {
-          console.log("3");
-          time = moment.utc(timer * 1000).format("h:mm:ss");
+          console.log('3');
+          time = moment.utc(timer * 1000).format('h:mm:ss');
         } else if (timer * 1000 < 3600000) {
-          time = moment.utc(timer * 1000).format("mm:ss");
+          time = moment.utc(timer * 1000).format('mm:ss');
         }
-        document.getElementById("demo").innerHTML = time;
-        timer = timer - 1;
+        document.getElementById('demo').innerHTML = time;
+        timer -= 1;
       } else {
-        document.getElementById("demo").innerHTML = timer;
+        document.getElementById('demo').innerHTML = timer;
         clearInterval(myTimer);
         can = false;
         return can;
